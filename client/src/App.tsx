a9954e614949474030f99e943d6a212a5fc83d11
samuel wanyoike <wanyoikesamuel42@gmail.com>
2024-10-10 01:47:49 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+function App() {
+
+  return (
+    <Router>
+      <div >
+        <Routes>
+          <Route path='/' element={<h1> Dashboard</h1>}/>
+          <Route path='/auth' element={<h1>Sign In</h1>}/>
+        </Routes>
+
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,8 @@
+
+const Auth = () => {
+  return (
+    <h1>Auth</h1>
+  )
+}
+
+export default Auth
